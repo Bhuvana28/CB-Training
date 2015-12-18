39ab2af268646568fb0173443a34193a99e4698c
Bhuvana28 <bhuvana@chargebee.com>
2015-12-18 12:46:49 +0530

    Day 1 files

---
+++ ConditionalStatement.java
@@ -0,0 +1,14 @@
+//Conditional Statement program.
+
+class ConditionalStatement{
+	
+	public static void main(String args[]){
+		boolean result = true;
+		if(result){
+			System.out.println("Result is true.");
+		}else{
+			System.out.println("Result is False.");
+		}
+	}
+
+}
