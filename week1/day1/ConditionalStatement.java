040524d3fcd7fc2adf39eb7ecb7e68a111a89245
Bhuvana28 <bhuvana@chargebee.com>
2015-12-19 11:25:14 +0530

    Conditional Statement java file changed

--- week1/day1/ConditionalStatement.java
+++ week1/day1/ConditionalStatement.java
@@ -1,13 +1,24 @@
 //Conditional Statement program.
 
+import java.util.Scanner;
+
 class ConditionalStatement{
 	
 	public static void main(String args[]){
-		boolean result = true;
-		if(result){
-			System.out.println("Result is true.");
+		int x,y,z;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter three numbers:");
+		x = scanner.nextInt();
+		y = scanner.nextInt();		
+		z = scanner.nextInt();
+		
+		//Find minimum of x,y,z
+		if(x < y && x < z){
+			System.out.println("Minimum is " + x);
+		}else if(y < x && y < z){
+			System.out.println("Minimum is " + y);
 		}else{
-			System.out.println("Result is False.");
+			System.out.println("Minimum is " + z);
 		}
 	}
 
