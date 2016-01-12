54c9e5ba2ada56f25f776dbddc84a24621edb2bb
cb-Bhuvana <bhuvana@chargebee.com>
2016-01-12 18:29:42 +0530

    week2 day5 file

--- week2/day5/miniApplication/TransformToConfigFileFormat.java
+++ week2/day5/miniApplication/TransformToConfigFileFormat.java
@@ -10,22 +10,25 @@ import org.json.simple.parser.ParseException;
 
 import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
 public class TransformToConfigFileFormat{
 
 	public static void transformValueFormat(CSVRecord record){
+
 		JSONParser parser = new JSONParser();
 
 		try{
 			JSONObject obj = (JSONObject)parser.parse(new BufferedReader(new FileReader("miniApplication/config1.json"))); 
-			LinkedMap<String,String> csvRecordMap = (LinkedMap<String,String>)record.toMap();
+			HashMap<String,String> csvRecordMap = (HashMap<String,String>)record.toMap();
 
 			ArrayList<String> formattedValues = new ArrayList<String>();
 
 			for(String heading : csvRecordMap.keySet()){
-				String jsonValue;
-				jsonValue = (String)obj.get(heading);	
+				//JSONObject jsonValue;
+				//jsonValue = (String)obj.get(heading);	
 				System.out.println(heading);
+
 			}
 
 		}catch (FileNotFoundException e) {  
@@ -34,7 +37,7 @@ public class TransformToConfigFileFormat{
   		}catch (IOException e) {  
    			e.printStackTrace();  
   		}catch (ParseException e) {  
-   		//	e.printStackTrace();  
+   			e.printStackTrace();  
   		}		  
 	}
 	
