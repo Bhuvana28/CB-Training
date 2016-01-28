4cf1e58489e6e4c5a926e5e40753e6a2f0df873f
cb-Bhuvana <bhuvana@chargebee.com>
2016-01-28 12:01:19 +0530

    day 4 changes done

--- week3/day4/alter-trigger-fns.sql
+++ week3/day4/alter-trigger-fns.sql
@@ -1,39 +1,12 @@
 alter table marks add column created_at datetime default now();
-alter table marks add column updated_at datetime default now();
+alter table marks modify column updated_at datetime default now() on update now();
 
 alter table students add column created_at datetime default now();
-alter table students add column updated_at datetime default now();
+alter table students modify column updated_at datetime default now() on update now();
 
 alter table medals add column created_at datetime default now();
-alter table medals add column updated_at datetime default now();
+alter table medals modify column updated_at datetime default now() on update now();
 
-drop trigger if exists updatedatetimemarks;
-delimiter $$
-create trigger updatedatetimemarks before update on marks for each row
-	begin
-	set new.updated_at = now();
-	end
-	$$
-	delimiter ;
-
-drop trigger if exists updatedatetimestudents;
-delimiter $$
-create trigger updatedatetime before update on students for each row
-	begin
-	set new.updated_at = now();
-	end
-	$$
-	delimiter ;
-
-
-drop trigger if exists updatedatetimemedals;
-delimiter $$
-create trigger updatedatetimemedals before update on medals for each row
-	begin
-	set new.updated_at = now();
-	end
-	$$
-	delimiter ;
 
 update marks set quarterly = 0 where quarterly is null;
 update marks set half_yearly = 0 where half_yearly is null;
@@ -53,37 +26,8 @@ create table if not exists students_summary(
 	constraint students_summary_id_fk foreign key(student_id) references students(id)
 );
 
-drop procedure if exists setStudentSummary;
-delimiter $$
-create procedure setStudentSummary()
-	begin
-	declare stud_id bigint(19);
-	declare stud_name varchar(100);
-	declare stud_year int(11);
-	declare stud_percentage float(4,2);
-	declare no_medals int(2);
-	declare finished int default 0;
-
-	declare cur1 cursor for select s.id,s.name,ma.year,round((sum(COALESCE(ma.annual,0))/5),2),COALESCE(me.medal_won,0) from marks ma right join students s on s.id=ma.student_id left join (select student_id,count(medal_won) 'medal_won',year from medals group by student_id,year) me on me.student_id=ma.student_id and me.year = ma.year group by s.id,s.name,ma.year,me.medal_won;
-	declare continue handler for not found set finished = 1;
-
-	open cur1;
-
-	summary: loop
-		fetch cur1 into stud_id,stud_name,stud_year,stud_percentage,no_medals;
-		if finished=1 then
-			leave summary;
-		end if;
-		insert into students_summary values(stud_id,stud_name,stud_year,stud_percentage,no_medals);
-	end loop summary;
-
-	close cur1;
-
-	end $$
-delimiter ;
-
-call setStudentSummary();
-
+insert into students_summary(student_id,student_name,year,percentage,no_of_medals_received) select s.id,s.name,ma.year,round((sum(IFNULL(ma.annual,0))/5),2),COALESCE(me.medal_won,0) from marks ma right join students s on s.id=ma.student_id left join (select student_id,count(medal_won) 'medal_won',year from medals group by student_id,year) me on me.student_id=ma.student_id and me.year = ma.year group by s.id,s.name,ma.year,me.medal_won;
+	
 ----
 alter table marks add column average float(6,2) not null default 0;
 
@@ -108,33 +52,35 @@ create trigger setAverage before insert on marks for each row
 
 alter table medals add column medal_received varchar(10);
 
+drop trigger if exists setMedalWonToMedalReceived;
 delimiter $$
 create trigger setMedalWonToMedalReceived before insert on medals for each row
 	begin 
-	set new.medal_received = new.medal_won;
+	if new.medal_received is not null THEN
+		set new.medal_won = new.medal_received;
+	elseif new.medal_won is not null THEN
+		set new.medal_received = new.medal_won;
+	end if;
 	end
 	$$
 	delimiter ;
 
+drop trigger if exists updateMedalReceived;
 delimiter $$
 create trigger updateMedalReceived before update on medals for each row
 	begin
-	set new.medal_received = new.medal_won;
+	if new.medal_received<>old.medal_received THEN
+		set new.medal_won = new.medal_received;
+	elseif new.medal_won <> old.medal_won THEN
+		set new.medal_received = new.medal_won;
+	end if;
 	end
 	$$
 	delimiter ;
 
+update medals set medal_received = medal_won;
 
-drop procedure if exists copyMedalWonToMedalReceived;
-delimiter $$
-create procedure copyMedalWonToMedalReceived()
-	begin
-	update medals set medal_received = medal_won;
-	end$$
-	delimiter ;
-
-call copyMedalWonToMedalReceived();
-
+alter table medals drop column medals_received;
 
 
 
